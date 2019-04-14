019dec34e55f25abb861aa7971c5eb3e34cc1ed3
polacla1 <claus.polanka@s-itsolutions.at>
2019-04-14 12:01:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello Babs <br /> 
+            Hello Claus <br /> 
+            Hello Claus <br /> 
+            Hello Irene Polanka <br /> 
+            Hello Walter Polanka <br /> 
+            Hello Sepp<br /> 
+            Hello Polanka und Ebinger Family <br />
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
